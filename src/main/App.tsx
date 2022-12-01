acfedcafddee400959d4f24d7cf2db7a8bceb789
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-12-02 00:15:12 +0200

    path was modified and now it track current location,api on item was created, axios was installed, images was replaced, phone images was added, image card was implemented with adaptive and correct reload of data while take different colors or capacity, add logic was takken from product card, phone descr type was added

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from '../components/Header/Header';
+import { ItemCard } from '../components/ItemCard';
 import { Path } from '../components/Path';
 
 export const App = () => {
@@ -7,6 +8,7 @@ export const App = () => {
     <div className="app">
       <Header />
       <Path />
+      <ItemCard />
     </div>
   );
 }
